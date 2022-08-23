a63468bd1c1d8b2b889f17875f6a8ac00517e687
Cleiton <111751059+cleitonsilva14@users.noreply.github.com>
2022-08-23 14:38:56 -0300

    Update

--- HelloWorld/Program.fs
+++ HelloWorld/Program.fs
@@ -1,31 +1,10 @@
-﻿open System
+open System
+
 
 [<EntryPoint>]
 let main argv =
-
-    printfn "%A" argv[2]
-
-    let mutable a = "Abacaxi"
-    printfn "%s" a
-
-    let b = "Banana"
-    printfn "%s" b
-
-    a <- "Ameixa"
-    printfn "%s" a
-
-
-    let idade = 18
-    let PI = 3.14
-    let name = "Jhon"
-
-    let raio:float = 7.5
-    let on:bool = true
-
-    printfn $"{name}, {idade}"
-    printfn $"={PI**2}"
-    printfn "Hi, %s" name
-    printfn "Is on: %b" on
+    let argumentos = argv
+    for arg in argumentos do
+        printfn "%s" arg
+    printfn "%d" argumentos.Length
     0
-
-
