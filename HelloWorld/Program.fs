99377223427fec32fcc23a3efce6b1c230b7a673
Cleiton <111751059+cleitonsilva14@users.noreply.github.com>
2022-08-23 10:59:52 -0300

    Update

--- HelloWorld/Program.fs
+++ HelloWorld/Program.fs
@@ -1,1 +1,14 @@
-﻿printfn "Hello from F#"
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    
+    let mutable a = "Abacaxi"
+    printfn "%s" a
+
+    let b = "Banana"
+    printfn "%s" b
+
+    a <- "Ameixa"
+    printfn "%s" a
+    0
