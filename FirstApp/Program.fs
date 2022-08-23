202a5c22edbb8294027931f147f3da6e51af4628
Cleiton <111751059+cleitonsilva14@users.noreply.github.com>
2022-08-23 10:04:53 -0300

    Adicionado um console app First App

---
+++ FirstApp/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+// define uma função
+let from whom = 
+    sprintf "from %s" whom
+
+
+let main argv =
+    let message = from "F#" // chama a função
+    printf "Hello world %s" message
+    0 // exit code
