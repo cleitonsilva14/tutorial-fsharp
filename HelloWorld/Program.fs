de7e16b36821218912b008a59d1804563e210685
Cleiton <111751059+cleitonsilva14@users.noreply.github.com>
2022-08-31 09:05:11 -0300

    Nova função com lista

--- HelloWorld/Program.fs
+++ HelloWorld/Program.fs
@@ -1,8 +1,13 @@
 open System
 
 let somar a b =
-    let soma = a + b
+    let 
+    soma = a + b
     soma
+
+let dobrarValoresLista values =
+    values |> List.map (fun x -> x * x + 1)
+
 
 [<EntryPoint>]
 let main argv =
@@ -12,4 +17,8 @@ let main argv =
     let s = somar x y
     printfn "%d" s
 
+    let numbers = [1;2;3;4;5]
+    let result = dobrarValoresLista numbers
+
+    printfn "%d" result
     0
